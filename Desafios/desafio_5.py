6afa66d0fcbf4da2e866978763b7c4eed08b4333
Santiago-Farias <santifa8@gmail.com>
2024-07-12 13:16:45 -0300

    Se agregó el desafío número 5 'Contador ascendente del 1 al 10'

---
+++ Desafios/desafio_5.py
@@ -0,0 +1,12 @@
+""" Desafío N°5:
+Crear un programa que muestre en la consola 
+10 repeticiones con números ASCENDENTE desde el 1 al 10
+Compartí el desafío en el foro de tu división en la sección Foro de comunicación -> Consultas. """
+
+num = 0
+
+while num < 10:
+    print("Voy por la flexión " + str(num+1))
+    num += 1
+
+print(str(num) + " flexiones, toma pa vo'")
